9cef8f5cea9462edc9360011e471c797ff3ea7c0
Steven Bertolucci <steven.bertolucci@yahoo.com>
2023-08-31 14:53:42 -0700

    Added a stress test. Still needs work to do

--- sort.c
+++ sort.c
@@ -4,6 +4,7 @@
 // Description: This file sorts arrays in ascending order
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void sort(int nums[]) {
 
@@ -11,8 +12,8 @@ void sort(int nums[]) {
     int n = sizeof(nums) - 1;
 
     // Loop to sort the array
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
             // Is the current value greater than the next value?
             if (nums[i] > nums[j]) {
                 // If so, swap those values
@@ -27,6 +28,7 @@ void sort(int nums[]) {
 
 int main() {
     int numbers[] = {24, 1, 6, 21, 2, 9, 3};
+    int randomArray[1000];
 
     // Print the current array
     printf("Here are the numbers in the array: ");
@@ -43,6 +45,31 @@ int main() {
         printf("%d ", numbers[i]);
     }
 
+    printf("\n\n");
+
+    // Stress test
+    printf("Here is the randomized array of 1000 indices: ");
+    int length = sizeof(randomArray) / sizeof(randomArray[0]);
+    for (int i = 0; i < length + 1; i++) {
+        // Random integers will be between 0 - 100
+        randomArray[i] = rand() % 101;
+        printf("%d ", randomArray[i]);
+    }
+
     printf("\n");
+    printf("Number of indices: %d", length);
+
+    printf("\n\n");
+
+    // Print the sorted randomized array
+    sort(randomArray);
+    printf("Here is the randomized array sorted: ");
+    for (int i = 0; i < length; i++) {
+        printf("%d ", randomArray[i]);
+    }
+    printf("\n");
+    printf("Number of indices: %d", length);
+
+    printf("\n\n");
     return 0;
 }
