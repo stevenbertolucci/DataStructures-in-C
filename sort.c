bd20bf36fe8528cedca6b5c67b4415f8582b9d9c
Steven Bertolucci <steven.bertolucci@yahoo.com>
2023-08-31 14:27:32 -0700

    Added comments

--- sort.c
+++ sort.c
@@ -1,12 +1,21 @@
+// Author: Steven Bertolucci
+// Date: 8/31/23
+// File: sort.c
+// Description: This file sorts arrays in ascending order
+
 #include <stdio.h>
 
 void sort(int nums[]) {
 
+    // Save the number of indexes in the array
     int n = sizeof(nums) - 1;
 
+    // Loop to sort the array
     for (int i = 0; i < n; ++i) {
         for (int j = i + 1; j < n; ++j) {
+            // Is the current value greater than the next value?
             if (nums[i] > nums[j]) {
+                // If so, swap those values
                 int a = nums[i];
                 nums[i] = nums[j];
                 nums[j] = a;
